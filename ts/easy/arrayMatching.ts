fddafcc334463f9e3d3e207164ab50ecc006100d
mraees93 <raeesdolley76@gmail.com>
2026-03-12 22:58:22 +0200

    debug arrayMatching

---
+++ ts/easy/arrayMatching.ts
@@ -0,0 +1,27 @@
+// For this challenge you will add elements from two arrays in a particular order.
+// have the function ArrayMatching(strArr) read the array of strings stored in strArr which will contain only two 
+// elements, both of which will represent an array of positive integers. For example: if strArr is 
+// ["[1, 2, 5, 6]", "[5, 2, 8, 11]"], then both elements in the input represent two integer arrays, and your goal 
+// for this challenge is to add the elements in corresponding locations from both arrays. For the example input, 
+// your program should do the following additions: [(1 + 5), (2 + 2), (5 + 8), (6 + 11)] which then equals [6, 4, 13, 17].
+//  Your program should finally return this resulting array in a string format with each element separated by a 
+// hyphen: 6-4-13-17. If the two arrays do not have the same amount of elements, then simply append the remaining 
+// elements onto the new array(example shown below).Both arrays will be in the format : [e1, e2, e3, ...] where at 
+// least one element will exist in each array.
+
+function ArrayMatching(strArr: string[]) {
+    const arr1 = strArr[0].match(/[0-9]/g)?.map(Number);
+    const arr2 = strArr[1].match(/[0-9]/g)?.map(Number);
+
+    const sortedArrays = [arr1, arr2].sort((a, b) => b.length - a.length);
+
+    let i = 0;
+    while(sortedArrays[1][i] !== undefined) {
+        sortedArrays[0][i] = sortedArrays[0][i] + sortedArrays[1][i];
+        i++
+    }
+
+    return sortedArrays[0]
+}
+console.log(ArrayMatching(["[1, 2, 5, 6]", "[5, 2, 8, 11]"]));
+console.log(ArrayMatching(["[5, 2, 3]", "[2, 2, 3, 10, 6]"]));
